9e5a2f0e4a5ac85de76f4c28257d98380ddcf7a5
C19 <classone2010@gmail.com>
2017-10-08 17:03:06 +0800

    finish Clock

--- clock/src/Clock.hs
+++ clock/src/Clock.hs
@@ -1,15 +1,31 @@
-module Clock (clockHour, clockMin, fromHourMin, toString) where
+{-# LANGUAGE TypeSynonymInstances #-}
 
-data Clock = Dummy
+module Clock (clockHour, clockMin, fromHourMin, toString) where
+import Text.Printf (printf)
+
+newtype Clock = Clock Int deriving (Show, Eq)
+
+instance Num Clock where
+  abs = id
+  signum _ = Clock 1
+  (*) (Clock a) (Clock b) = fromMin $ a * b
+  (+) (Clock a) (Clock b) = fromMin $ a + b
+  negate (Clock clock) = fromMin $ 24 * 60 - clock
+  fromInteger i = fromMin (fromInteger i)
 
 clockHour :: Clock -> Int
-clockHour clock = error "You need to implement this function."
+clockHour (Clock clock) = (clock `div` 60) `mod` 24
 
 clockMin :: Clock -> Int
-clockMin clock = error "You need to implement this function."
+clockMin (Clock clock) = clock `mod` 60
 
 fromHourMin :: Int -> Int -> Clock
-fromHourMin hour min = error "You need to implement this function."
+fromHourMin hour min = fromMin (hour * 60 + min)
+
+fromMin :: Int -> Clock
+fromMin min
+  | min < 0 = fromMin $ min + 24 * 60
+  | otherwise = Clock (min `mod` (24 * 60))
 
 toString :: Clock -> String
-toString clock = error "You need to implement this function."
+toString clock = Text.Printf.printf "%02d:%02d" (clockHour clock) (clockMin clock)
