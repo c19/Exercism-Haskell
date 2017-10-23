a0c550d897bd013a9c6183cc4367457e2342170a
C19 <classone2010@gmail.com>
2017-10-23 20:05:34 +0800

    rotationalcipher

--- rotational-cipher/src/RotationalCipher.hs
+++ rotational-cipher/src/RotationalCipher.hs
@@ -1,4 +1,16 @@
 module RotationalCipher (rotate) where
 
+conv :: Int -> Char -> Char
+conv k c
+  | 65 <= i && i <= 90 = rotate' 65 k c
+  | 97 <= i && i <= 122 = rotate' 97 k c
+  | otherwise = c
+  where i = fromEnum c
+
+rotate' :: Int -> Int -> Char -> Char
+rotate' base k c = toEnum $ (i + k - base) `mod` 26 + base :: Char
+    where base = if i < 97 then 65 else 97
+          i = fromEnum c
+
 rotate :: Int -> String -> String
-rotate = error "You need to implement this function."
+rotate k = map (conv k)
