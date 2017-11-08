b00d60754c7a6f284c3b3b4c922d15f846b5236e
C19 <classone2010@gmail.com>
2017-11-08 20:42:27 +0800

    pascals triangle

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,4 +1,16 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows x = [row (toInteger i) | i <- [0..(x-1)]]
+
+row :: Integer -> [Integer]
+row 0 = [1]
+row n = [combine n i | i <- [0..n]]
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = product [1..n]
+
+combine :: Integer -> Integer -> Integer
+combine n 0 = 1
+combine n k = ((factorial n) `div` (factorial k)) `div` (factorial (n - k))
